682c4b29bab1e1e1e9db913561a9141ff5ced744
sulaimondawood <sulaimondawod@gmail.com>
2022-12-13 12:16:35 +0100

    first commit

---
+++ pages/sign-in.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "../styles/screens/sign.module.css";
+const SignIn = () => {
+  return <div></div>;
+};
+
+export default SignIn;
---
+++ src/components/Input.tsx
@@ -0,0 +1,4 @@
+const Input = () => {
+  return <h1>Input</h1>;
+};
+export default Input;
